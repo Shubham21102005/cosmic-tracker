47ad932923f39d770351a352ae625aab2961e3b8
Shubham <krshubham21102005@gmail.com>
2025-08-18 01:23:02 +0530

    Small change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -172,10 +172,12 @@ export default function HomePage() {
           <EventSection key={date} date={date} events={filteredFeed[date]} />
         ))}
 
+
+
       {/* Conditionally render the "Load More" button */}
       {!isLoading && !endDate && sortedDates.length > 0 && (
         <div className="text-center mt-8">
-          <button
+          <button //test 
             onClick={handleLoadMore}
             disabled={isLoadingMore}
             className="px-6 py-2 bg-slate-700 text-white font-semibold rounded-lg hover:bg-slate-600 disabled:bg-slate-500 disabled:cursor-not-allowed"
